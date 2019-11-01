fc51b913d79053493d8fec1d82e801813eba9d8b
Nicole Lucien <nicolemlucien@gmail.com>
2019-11-01 18:26:59 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,7 +16,7 @@ class String
 
   def count_sentences
     new_array = self.split(/[.?!]/).reject {|string| string.empty?}
-    binding.pry
+    # binding.pry
     return new_array.length
   end
 end
